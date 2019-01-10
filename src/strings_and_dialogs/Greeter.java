e9507636b363bdbbcaf2a59a05338b672cedfd0c
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-01-09 18:28:21 -0800

    Save File

--- src/strings_and_dialogs/Greeter.java
+++ src/strings_and_dialogs/Greeter.java
@@ -1,7 +1,12 @@
 package strings_and_dialogs;
 
+import java.nio.channels.ShutdownChannelGroupException;
+
+import javax.swing.JOptionPane;
+
 public class Greeter {
 	public static void main(String[] args) {
-		
+		String name = JOptionPane.showInputDialog("Whats your name?");
+		System.out.println(name);
 	}
 }
